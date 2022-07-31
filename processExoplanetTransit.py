a9cc3a00c3d5cfe530a4cc62df83569148905289
Michael Primm <mprimm@mirificsystems.com>
2022-07-30 20:14:13 -0500

    Switch to just calibrating and solving images

--- processExoplanetTransit.py
+++ processExoplanetTransit.py
@@ -7,10 +7,11 @@ from astropy.io import fits
 from astropy.wcs import WCS, FITSFixedWarning
 from astropy.coordinates import SkyCoord
 import cv2
-import csv
 import numpy as np
 import subprocess
 import warnings
+
+from pandas import isna
 
 warnings.simplefilter('ignore', category=FITSFixedWarning)
 
@@ -31,38 +32,11 @@ def runsolving(ra, dec, infile, outfile):
         print("Timeout solving %s - skipping" % f)
         return False
 
-def runstacking(ra, dec, fitsfiles, stackfile, mjdobs, mjdend): 
-    print("Stacking %d images into %s" % (len(fitsfiles), stackfile))
-    tmpst = os.path.join(tmppath, "tmpstack.fits")
-    stackargs = [ "SWarp", 
-        "-IMAGEOUT_NAME", tmpst, 
-        "-WRITE_XML", "N",
-#        "-CENTER_TYPE", "MANUAL",
-#        "-CENTER", "%f,%f" % (ra, dec),
-        "-RESAMPLE_DIR", tmppath,
-        "-COPY_KEYWORDS", "OBJECT,ORIGIN,MINSYET,TELESCOP,INSTUME,SERIALNB,TIMEUNIT,LATITUDE,LONGITUD,GAIN,GAINDB,ALTITUDE,CMOSTEMP,OBSMODE,DATE,SOFTVER" ]                              
-    stackargs.extend(fitsfiles)
-    rslt = subprocess.run(stackargs, capture_output=True)
-    if rslt.returncode != 0:
-        print("Error stacking %s" % stackfile)
-        return False
-    # And resolve new file
-    if runsolving(ra, dec, tmpst, stackfile):
-        # And add MJD fields for stack
-        with fits.open(stackfile) as hduList:
-            hduList[0].header['MJD-OBS'] = mjdobs
-            hduList[0].header['MJD-END'] = mjdend
-            hduList[0].header['MJD-MID'] = (mjdobs + mjdend) / 2
-            hduList.writeto(stackfile, overwrite=True)
-            w = WCS(hduList[0].header)
-            x, y = w.world_to_pixel(fov)
-            print("Stacked file %s: target at pixel %f, %f" % (stackfile, x, y))
-        return True
-    return False
 # Initialize parser
 parser = argparse.ArgumentParser()
 # Add input argument
-parser.add_argument("csvfile", help = "Input CSV file") 
+parser.add_argument("-d", "--darks", help = "Dark files source directory");
+parser.add_argument("-s", "--science", help = "Science files source directory");
 # Adding output argument
 parser.add_argument("-o", "--output", help = "Output directory") 
 # Add flags (default is grey - others for monochrome)
@@ -70,30 +44,21 @@ parser.add_argument('-r', "--red", action='store_true')
 parser.add_argument('-g', "--green", action='store_true')
 parser.add_argument('-b', "--blue", action='store_true')
 parser.add_argument("-G", "--gray", action='store_true')
-# Add number of minutes to stack
-parser.add_argument("--stacktime", help = "Number of mintutes to stack (default 2)") 
-parser.add_argument("--presolve", action='store_true', help = "If set, solve subs before stacking")
-parser.add_argument("--keepsolved", action='store_true', help = "If set, keep solved subs after stacking")
 
 # Read arguments from command line
 try:
     args = parser.parse_args()
 except argparse.ArgumentError:
     os.exit(1)
-outputdir='.'
+outputdir='output'
 if args.output: 
     outputdir = args.output
-stacktime = 2
-if args.stacktime:
-    stacktime = int(args.stacktime)
-if args.csvfile is None:
-    print("CSV filename is required")
-    os.exit(1)
-if os.path.isfile(args.csvfile) == False:
-    print("CSV file not found: ", args.csvfile)
-    os.exit(1)
-basedir = os.path.dirname(args.csvfile)
-
+darksrcdir='darks'
+if args.darks:
+    darksrcdir = args.darks 
+sciencesrcdir='science'
+if args.science:
+    sciencesrcdir = args.science
 # Make output directory, if needed
 pathlib.Path(outputdir).mkdir(parents=True, exist_ok=True)
 darkpath = os.path.join(outputdir, "darks")
@@ -102,8 +67,6 @@ sciencepath = os.path.join(outputdir, "science")
 pathlib.Path(sciencepath).mkdir(parents=True, exist_ok=True)
 tmppath = os.path.join(outputdir, "tmp")
 pathlib.Path(tmppath).mkdir(parents=True, exist_ok=True)
-stackedpath = os.path.join(outputdir, "stacked")
-pathlib.Path(stackedpath).mkdir(parents=True, exist_ok=True)
 
 togray = False
 coloridx = 0
@@ -119,39 +82,31 @@ elif args.blue:
 else:
     togray = True
     print("Produce grayscale FITS files")
-dopresolve = False
-if args.presolve:
-    dopresolve = True
-dokeepsolved = False
-if args.keepsolved:
-    dokeepsolved = args.keepsolved;
 darkfiles = []
+# Go through the darks
+for path in os.listdir(darksrcdir):
+    dfile = os.path.join(darksrcdir, path)
+    # check if current path is a file
+    if os.path.isfile(dfile):
+        darkfiles.append(path)
+darkfiles.sort()
+# Go through the lights
 lightfiles = []
-with open(args.csvfile, newline='') as csvfile:
-    csvreader = csv.DictReader(csvfile)
-    for row in csvreader:
-        category = row['category']
-        if category == 'dark':
-            darkfiles.append(row['filename'])
-        elif category == 'science':
-            # Remember RA, DEC for first science frame - assume this is right
-            if (len(lightfiles) == 0):
-                fov = SkyCoord(float(row['fovra']), float(row['fovdec']), frame='icrs', unit='deg')
-            lightfiles.append(row['filename']) 
-            
-    print("CSV file has %d darks and %d science FITS files" % (len(darkfiles), len(lightfiles)))
-
-print("Center of FOV for first science: %s" % fov.to_string("dms"))
-
+for path in os.listdir(sciencesrcdir):
+    dfile = os.path.join(sciencesrcdir, path)
+    # check if current path is a file
+    if os.path.isfile(dfile):
+        lightfiles.append(path)
+lightfiles.sort()
 dark = fits.HDUList()
 
 # Build dark frame, if we have any to work with
 if len(darkfiles) > 0:
     for f in darkfiles:
         try:
-            fname = os.path.join(basedir, f)
+            dfile = os.path.join(darksrcdir, f)
             # Load file into list of HDU list 
-            with fits.open(fname) as hduList:
+            with fits.open(dfile) as hduList:
                 # Use first one as base
                 if len(dark) == 0:
                     darkaccum = np.zeros((0,) + hduList[0].data.shape)
@@ -178,9 +133,13 @@ stackedcnt = 0
 
 for f in lightfiles:
     try:
+        lfile = os.path.join(sciencesrcdir, f)
         # Load file into list of HDU list 
-        fname = os.path.join(basedir, f)
-        with fits.open(fname) as hduList:
+        with fits.open(lfile) as hduList:
+            # First science? get center as target
+            if (cnt == 0):
+                fov = SkyCoord(hduList[0].header['FOVRA'], hduList[0].header['FOVDEC'], frame='icrs', unit='deg')
+                print("center of FOV for first science: RA={0}, DEC={1}".format(hduList[0].header['FOVRA'], hduList[0].header['FOVDEC']))
             # First, calibrate image
             if len(dark) > 0:
                 # Clamp the data with the dark from below, so we can subtract without rollover
@@ -198,69 +157,32 @@ for f in lightfiles:
                 for idx, val in enumerate(dst):
                     hduList[0].data[idx] = val[:,coloridx]
             rslt = True
-            newfits = os.path.join(sciencepath, f)
-            if dopresolve:
-                # Write to temporary file so that we can run solve-field to
-                # set WCS data
-                hduList.writeto(os.path.join(tmppath, "tmp.fits"), overwrite=True)
-                # Now run solve-field to generate final file
-                rslt = runsolving(hduList[0].header['FOVRA'], hduList[0].header['FOVDEC'],
-                    os.path.join(tmppath, "tmp.fits"), newfits )
-                if rslt == True:
-                    # Read new file - see if we are still in frame
-                    with fits.open(newfits) as hduListNew:
-                        w = WCS(hduListNew[0].header)
-                        shape = hduList[0].data.shape
-                        x, y = w.world_to_pixel(fov)
-                        print("Solved %s:  target at %f, %f" % (newfits, x, y))
-                        # If out of range, drop the frame
-                        if (x < 0) or (x >= shape[0]) or (y < 0) or (y >= shape[1]):
-                            rslt = False;
-                            print("Discarding %s - target out of frame" % fname)                    
-            else:   # No presolve
-                hduList.writeto(newfits, overwrite=True)
+            newfits = os.path.join(sciencepath, "science-{0:05d}.fits".format(cnt))
+            # Write to temporary file so that we can run solve-field to
+            # set WCS data
+            hduList.writeto(os.path.join(tmppath, "tmp.fits"), overwrite=True)
+            # Now run solve-field to generate final file
+            rslt = runsolving(hduList[0].header['FOVRA'], hduList[0].header['FOVDEC'],
+                os.path.join(tmppath, "tmp.fits"), newfits )
+            if rslt == True:
+                # Read new file - see if we are still in frame
+                with fits.open(newfits) as hduListNew:
+                    w = WCS(hduListNew[0].header)
+                    shape = hduList[0].data.shape
+                    x, y = w.world_to_pixel(fov)
+                    print("Solved %s:  target at %f, %f" % (newfits, x, y))
+                    # If out of range, drop the frame
+                    if (x < 0) or (x >= shape[0]) or (y < 0) or (y >= shape[1]):
+                        rslt = False;
+                        print("Discarding %s - target out of frame" % fname)                    
             if rslt == False:
                 print("Error solving %s - skipping" % f)
             else:
                 tobs = hduList[0].header['MJD-OBS'] * 24 * 60 # MJD in minutes
-                # End of accumulator?
-                if (len(timeaccumlist) > 0) and ((timeaccumstart + stacktime) < tobs):
-                    stackout = os.path.join(stackedpath, "stack-%04d.fits" % stackedcnt)
-                    tmpout = os.path.join(tmppath, "tmpstack.fits")
-                    try: 
-                        runstacking(timeaccumra, timeaccumdec, timeaccumlist, stackout, mjdobs, mjdend)
-                        stackedcnt = stackedcnt + 1
-                    except OSError as e:
-                        print("Error: stacking file %s - %s (%s)" % (stackout, e.__class__, e))   
-                    if dokeepsolved == False:
-                        for f in timeaccumlist:
-                            os.remove(f)  
-                    timeaccumlist = []
-                    timeaccumstart = 0
-                else:
-                    # If first one to accumulate, save start time and RA/Dec
-                    if (len(timeaccumlist) == 0):
-                        timeaccumstart = tobs
-                        timeaccumra = hduList[0].header['FOVRA']
-                        timeaccumdec = hduList[0].header['FOVDEC']
-                        mjdobs = hduList[0].header['MJD-OBS']
-                    timeaccumlist.append(newfits)
-                    mjdend = hduList[0].header['MJD-END']
                 solvedcnt = solvedcnt + 1
             cnt = cnt + 1
     except OSError as e:
         print("Error: file %s - %s (%s)" % (f, e.__class__, e))     
-# Final accumulator?
-if len(timeaccumlist) > 0:
-    stackout = os.path.join(stackedpath, "stack-%04d.fits" % stackedcnt)
-    try: 
-        runstacking(timeaccumra, timeaccumdec, timeaccumlist, stackout, mjdobs, mjdend)
-        stackedcnt = stackedcnt + 1
-    except OSError as e:
-        print("Error: stacking file %s - %s (%s)" % (stackout, e.__class__, e))     
-    if dokeepsolved == False:
-        for f in timeaccumlist:
-            os.remove(f)  
 
 print("Processed %d out of %d files and %d stacks into destination '%s'" % (solvedcnt, cnt, stackedcnt, outputdir))
 
