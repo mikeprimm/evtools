870a42ab85c9ea7df40b4491e7cee31cd3f46178
Mike Primm <mike@primmhome.com>
2023-01-13 01:33:16 -0600

    Flip PNG input array

--- convertToColorPNG.py
+++ convertToColorPNG.py
@@ -46,9 +46,10 @@ for f in filelist:
         with fits.open(f) as hduList:
             basename = os.path.splitext(os.path.basename(f))[0]
             # Demosaic the image
-            dat = (hduList[0].data - np.min(hduList[0].data)) / (np.max(hduList[0].data) - np.min(hduList[0].data)) * 65535
+            dat = np.sqrt((hduList[0].data - np.min(hduList[0].data)) / (np.max(hduList[0].data) - np.min(hduList[0].data))) * 65535
             dat = dat.astype(np.uint16)
             dst = cv2.cvtColor(dat, cv2.COLOR_BayerRG2RGB)
+            dst = np.flip(dst, 0)  # Flip Y axis for PNG
             cv2.imwrite(os.path.join(outputdir, "%s.png" % basename), dst)
         cnt = cnt + 1
     except OSError:
