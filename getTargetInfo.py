3ed44e0581f3d8a6d430a585a08a503a0474dea6
Mike Primm <mike@primmhome.com>
2023-09-27 21:15:43 -0500

    Add support for manual RA, Dec and Vmag

--- getTargetInfo.py
+++ getTargetInfo.py
@@ -5,6 +5,8 @@ import logging
 from astropy.time import Time
 from datetime import datetime
 from math import log10, floor
+import astropy.units as u
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 try:
     from .libs.unistellar import unistellarBestGainAndExp, unstellarExoplanetURL
 except ImportError:  # package import
@@ -25,8 +27,12 @@ logger.addHandler(ch)
 
 # Initialize parser
 parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--target", help = "Target Name", required = True)
+parser.add_argument("-t", "--target", help = "Target Name")
 parser.add_argument("-d", "--duration", type=float)
+parser.add_argument("--ra", help = "Target RA")
+parser.add_argument("--dec", help = "Target Dec")
+parser.add_argument("--mag", help = "Magnitude (V)")
+
 # Read arguments from command line
 try:
     args = parser.parse_args()
@@ -36,23 +42,34 @@ except argparse.ArgumentError:
 duration = 1.0
 if (args.duration):
     duration = float(args.duration)
-
 logger.info("args=%s" % args)
 
-tic = exofop_getticid(args.target)
+if args.target:
+    tic = exofop_getticid(args.target)
+    logger.info(f"TIC={tic}")
+    if tic:
+        skypos, vmag = exofop_getcompositeinfo(tic)
+        # Adjust for proper motion (milliarcsec to degrees)
+        curpos = skypos.apply_space_motion(new_obstime = Time(datetime.utcnow(), scale='utc'))
+    else:
+        logger.error("Tsrget not found")
+        exit(1)
+elif args.dec and args.ra and args.mag:
+    skypos = SkyCoord(args.ra, args.dec, frame='icrs', unit=(u.hourangle, u.deg))
+    vmag = float(args.mag)
+    curpos = skypos
+else:
+    logger.error("Either --target or --ra, --dec and --mag required")
+    exit(1)
 
-logger.info(f"TIC={tic}")
-if tic:
-    skypos, vmag = exofop_getcompositeinfo(tic)
-    # Adjust for proper motion (milliarcsec to degrees)
-    curpos = skypos.apply_space_motion(new_obstime = Time(datetime.utcnow(), scale='utc'))
-    logger.info(f"J2000, epoch J2015.5: RA/Dec={skypos.to_string('hmsdms')}")
-    logger.info(f"Current RA/Dec={curpos.to_string('hmsdms')}")
-    logger.info(f"V (mag) = {vmag}")
-    bestgain, exptime = unistellarBestGainAndExp(vmag)
-    logger.info(f"Unistellar: best gain = {bestgain} db, exposure time = {exptime} ms")
-
-url = unstellarExoplanetURL(args.target, 10)
+logger.info(f"J2000, epoch J2015.5: RA/Dec={skypos.to_string('hmsdms')}")
+logger.info(f"Current RA/Dec={curpos.to_string('hmsdms')}")
+logger.info(f"V (mag) = {vmag}")
+bestgain, exptime = unistellarBestGainAndExp(vmag)
+logger.info(f"Unistellar: best gain = {bestgain} db, exposure time = {exptime} ms")
+if bestgain is None:
+    exit(1)
+url = f"unistellar://science/transit?ra={curpos.ra.deg:.5f}&dec={curpos.dec.deg:.5f}&c=3970&et={exptime}&g={bestgain}&d={int(10)}"
 logger.info(f"Unistellar URL (for 10 seconds): {url}")
-url = unstellarExoplanetURL(args.target, duration * 3600)
+url = f"unistellar://science/transit?ra={curpos.ra.deg:.5f}&dec={curpos.dec.deg:.5f}&c=3970&et={exptime}&g={bestgain}&d={int(duration * 3600)}"
 logger.info(f"Unistellar URL (for {duration} hours): {url}")
